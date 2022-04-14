6016a35de1ec47cb5c6bb9edf94c57df33cffeab
Vrund-Patel <90713163+Vrund-Patel@users.noreply.github.com>
2022-04-14 11:38:33 -0400

    Delete lab10_VrundPatel directory

--- lab10_VrundPatel/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab10_vrundpatel {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.lab10_vrundpatel to javafx.fxml;
-    exports com.example.lab10_vrundpatel;
-}
